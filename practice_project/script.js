b161ab200d47db98239fe80afe11856035305bd8
Ben Lauderbaugh <benjlaud1@gmail.com>
2017-04-15 08:14:42 -0500

    Completed boxes cycling through highlight class

--- practice_project/script.js
+++ practice_project/script.js
@@ -14,7 +14,7 @@ $(document).ready(onReady);
 //Function that runs when html is fully loaded:
 function onReady(){
   //click listener to button:
-  $('.change-highlight').on('click', highlightASquare)
+  $('.change-highlight').on('click', highlightASquare);
 
   //call to put starting squares on DOM
   appendSquaresToDom(numberOfSquares);
@@ -23,8 +23,29 @@ function onReady(){
 function highlightASquare(){
   //This variable stores an array of the elements with the 'square' class
   var arrayOfSquares = $('.square');
+  // arrayOfSquares.addClass('highlight');
 
-}
+  // check if any blocks have the highlight class
+  if ($('.container').children().hasClass('highlight')) {
+    // loop through and check each box has the highlight class
+    for (var i = 0; i < arrayOfSquares.length; i++) {
+      // check if box at i has class highlight
+      if($(arrayOfSquares[i]).hasClass('highlight')) {
+        $(arrayOfSquares[i]).removeClass('highlight');
+        if (i === arrayOfSquares.length - 1) {
+          $(arrayOfSquares[0]).addClass('highlight');
+        } else {
+          $(arrayOfSquares[i+=1]).addClass('highlight');
+        } // end if else i === arrayOfSquares.length
+        // Jump out of funciton
+        return;
+      } // end if $(arrayOfSquares[i]).hasClass('highlight')
+    } // end for loop
+  } // end if $('container').children().hasClass('highlight')
+  else {
+    $(arrayOfSquares[0]).addClass('highlight');
+  } // end else
+} // end highlightASquare
 
 
 //You don't need to change this function:
